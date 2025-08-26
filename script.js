a188f1d4fa90717910c3ace8b84510469164f3ee
Hellen Skrzypczak <skr.hellen@gmail.com>
2025-08-26 15:32:27 -0300

    Adiciona campo de comentários

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 $(document).ready(function() { //espera que toda a página carregue antes de executar o código
+    function carregarPosts(callbak, userId, valorFiltro) {
+        $.ajax({
+            url: `https://jsonplaceholder.typicode.com/posts`,
+            method: "GET",
+            success: function(posts){
+                callbak(posts, userId, valorFiltro);
+            },
+            error: function() {
+                $('#listaPosts').html("<li>Erro ao carregar os posts. Tente novamente.</li>");
+            }
+        });
+    }
 
     $('#btnBuscarPosts').click(function(){
         const userId = $("#inputUserId").val();
@@ -7,29 +19,79 @@ $(document).ready(function() { //espera que toda a página carregue antes de exe
             alert('Informe um valor entre 1 e 10.')
             return;
         }
+        carregarPosts(function(posts, userId) {
+            $('#listaPosts').empty(); //limpa a lista
+
+            posts.forEach(post => {
+                if(post.userId == userId)
+                {
+                    const html =`
+                        <li>
+                            <strong>Título</strong>: ${post.title}<br>
+                            <strong>Post</strong>: ${post.body}<br>
+                            <button class="btnVerComentarios" data-postid="${post.id}">Ver comentários</button><br>
+                            <ul class="comentarios"></ul><br>
+                        </li>
+                    `;
+                    $('#listaPosts').append(html);
+                };
+            });  
+        }, userId);
+    });
+
+    $('#btnLimpar').click(function(){
+        $('#listaPosts').empty();
+        $('#inputUserId').val("");
+    })
+
+    $('#listaPosts').on('click', '.btnVerComentarios', function(){
+        const idComentario = $(this).data('postid');
+        const li = $(this).closest('li'); //closest pega a tag ancestral
 
         $.ajax({
-            url: `https://jsonplaceholder.typicode.com/posts`,
-            method: "GET",
-            success: function(posts){
-                $('#listaPosts').empty(); //limpa a lista
-                posts.forEach(post => {
-                    if(post.userId == userId)
-                    {
-                        //string de HTML
-                        const html =`
+            url: `https://jsonplaceholder.typicode.com/comments`,
+            method: 'GET',
+            success: function(comentarios){
+                comentarios.forEach(c => {
+                    if (c.postId == idComentario){
+                        li.find('.comentarios').append(`
                             <li>
-                                <strong>Título</strong>: ${post.title}<br>
-                                <strong>Post</strong>: ${post.body}<br>
+                                <strong>${c.name}</strong>: ${c.body}
                             </li>
-                        `;
-                        $('#listaPosts').append(html);
-                    };
-                })
+                        `);
+                    }
+                });
+            },
+            error: function() {
+                $('.comentarios').html("<li>Erro ao carregar os comentarios. Tente novamente.</li>");
             }
         })
     })
 
+    function filtrarPosts(posts, valorFiltro){
+       $('#listaPosts').empty();
+        posts.forEach(post => {
+            if(post.title.includes(valorFiltro)){
+                const html =`
+                    <li>
+                        <strong>Título</strong>: ${post.title}<br>
+                        <strong>Post</strong>: ${post.body}<br>
+                        <button class="btnVerComentarios" data-postid="${post.id}">Ver comentários</button><br>
+                        <ul class="comentarios"></ul><br>
+                    </li>
+                `;
+                $('#listaPosts').append(html);
+            }
+            else
+            {
+                alert('Título inexistente!')
+            }
+       });
+    }
+
+    $('#btnFiltro').click(() => {
+        const valorFiltro = $('#inputFiltro').val();
+        filtrarPosts(valorFiltro);
+    })
 
 })
-
